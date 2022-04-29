a9f5adc600d418f8e591fb397e3558ec47f62e43
ikaslea <ikaslea@DESKTOP-O2E2ROF>
2022-04-29 14:30:07 +0200

    commit prueba

---
+++ pizzeria/src/pizzeria/main.java
@@ -0,0 +1,19 @@
+package pizzeria;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class main {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/pizzeria", "root", "");
+		
+		
+		
+		
+		
+	}
+
+}
